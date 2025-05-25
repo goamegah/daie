e99d1cdf56b713fb0f8dde223bb9ff64b7069fee
komlan godwin amegah <komlan.godwin.amegah@gmail.com>
2025-05-25 14:53:58 +0200

    update spark utils

--- tests/utils/test_spark_utils.py
+++ tests/utils/test_spark_utils.py
@@ -3,26 +3,26 @@
 import pytest
 from unittest.mock import MagicMock
 
-import mds.utils.spark_utils as su
+import mds.utils.spark_utils as SU
 from pyspark.sql import SparkSession
 
 # Fixture pour réinitialiser le singleton avant chaque test
 @pytest.fixture(autouse=True)
 def reset_spark_manager():
     # Réinitialise la SparkSession et DBUtils internes
-    su._spark_manager._spark = None
-    su._spark_manager._dbutils = None
+    SU._spark_manager._spark = None
+    SU._spark_manager._dbutils = None
     yield
     # Détruit la session Spark si elle existe
-    if su._spark_manager._spark is not None:
-        su._spark_manager._spark.stop()
-        su._spark_manager._spark = None
+    if SU._spark_manager._spark is not None:
+        SU._spark_manager._spark.stop()
+        SU._spark_manager._spark = None
 
 def test_get_spark_session_returns_singleton():
     # 1ère récupération
-    spark1: SparkSession = su.get_spark_session()
+    spark1: SparkSession = SU.get_spark_session()
     # 2ème récupération – même instance !
-    spark2: SparkSession = su.get_spark_session()
+    spark2: SparkSession = SU.get_spark_session()
     assert spark1 is spark2
     # On peut créer un DataFrame local sans erreur
     df = spark1.createDataFrame([(1, "a")], ["id", "val"])
@@ -34,9 +34,9 @@ def test_scope_exists_true(monkeypatch):
     """
     fake_dbutils = MagicMock()
     fake_dbutils.secrets.get.return_value = "secret"
-    monkeypatch.setattr(su, "get_dbutils", lambda: fake_dbutils)
+    monkeypatch.setattr(SU, "get_dbutils", lambda: fake_dbutils)
 
-    assert su.scope_exists("my_scope", "my_key") is True
+    assert SU.scope_exists("my_scope", "my_key") is True
     fake_dbutils.secrets.get.assert_called_once_with(scope="my_scope", key="my_key")
 
 def test_scope_exists_false(monkeypatch):
@@ -45,9 +45,9 @@ def test_scope_exists_false(monkeypatch):
     """
     fake_dbutils = MagicMock()
     fake_dbutils.secrets.get.side_effect = Exception("not found")
-    monkeypatch.setattr(su, "get_dbutils", lambda: fake_dbutils)
+    monkeypatch.setattr(SU, "get_dbutils", lambda: fake_dbutils)
 
-    assert su.scope_exists("my_scope", "my_key") is False
+    assert SU.scope_exists("my_scope", "my_key") is False
 
 
 if __name__ == "__main__":
