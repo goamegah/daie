49fdef6471c513b72e6bf32196f7971f7693afef
sikhouane <sikhouane@myges.fr>
2025-12-25 21:36:26 +0100

    Update

--- constants.py
+++ constants.py
@@ -1,8 +1,18 @@
-# bronze layer
-BRONZE_ROOT = "dbfs:/tmp/bronze/accidents"
+BRONZE_ROOT = "data/bronze/accidents/2023"
 
-# silver layer
-SILVER_ROOT = "dbfs:/tmp/silver/accidents"
+SILVER_ROOT = "data/silver/accidents/2023"
 
-# gold layer
-GOLD_ROOT   = "dbfs:/tmp/gold/accidents"
+GOLD_ROOT   = "data/gold/accidents/2023"
+
+SEP = ";"
+BRONZE_ROOT = BASE_DIR / "data" / "bronze"
+SILVER_ROOT = BASE_DIR / "data" / "silver"
+WAREHOUSE_DIR = str(BASE_DIR / "spark_warehouse")
+
+
+DATASETS = {
+    "caract": "caract-2023.csv",
+    "lieux": "lieux-2023.csv",
+    "usagers": "usagers-2023.csv",
+    "vehicules": "vehicules-2023.csv",
+}
--- spark_session.py
+++ spark_session.py
@@ -1,14 +1,38 @@
+import os
+from pathlib import Path
 from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+from pyspark.sql import functions as F
 
-spark = SparkSession.builder.getOrCreate()
+SEP = ";"
+BASE_DIR = Path(__file__).resolve().parents[2]
+BRONZE_ROOT = BASE_DIR / "data" / "bronze"
+SILVER_ROOT = BASE_DIR / "data" / "silver"
+WAREHOUSE_DIR = str(BASE_DIR / "spark_warehouse")
 
-def get_dbutils(spark):
-        try:
-            from pyspark.dbutils import DBUtils
-            dbutils = DBUtils(spark)
-        except ImportError:
-            import IPython
-            dbutils = IPython.get_ipython().user_ns["dbutils"]
-        return dbutils
 
-dbutils = get_dbutils(spark)
+DATASETS = {
+    "caract": "caract-2023.csv",
+    "lieux": "lieux-2023.csv",
+    "usagers": "usagers-2023.csv",
+    "vehicules": "vehicules-2023.csv",
+}
+
+def get_spark(app_name: str):
+    from pyspark.sql import SparkSession
+    from delta import configure_spark_with_delta_pip
+
+    builder = (
+        SparkSession.builder
+        .appName(app_name)
+        .master("local[*]")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.sql.warehouse.dir", WAREHOUSE_DIR)
+    )
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+    print("Spark:", spark.version)
+    print("Scala:", spark.sparkContext._jvm.scala.util.Properties.versionNumberString())
+    return spark
