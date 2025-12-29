c2594e1ca0e597cb03a9d5c2664e54283521c490
goamegah <komlan.godwin.amegah@gmail.com>
2025-12-29 01:40:47 +0100

    Add newline for better readability in grant_access_to_catalog.py

--- daie/jobs/ddl/grant_access_to_catalog.py
+++ daie/jobs/ddl/grant_access_to_catalog.py
@@ -1,10 +1,11 @@
 import sys
 from daie.utils import spark_utils as su
 
+
 def grant_access_to_catalog() -> None:
     dbw_env = su.get_main_env()
     su.spark.sql(f"GRANT USAGE, READ_METADATA, SELECT ON CATALOG hive_metastore TO `G-DE-daie-chn-{dbw_env}`")
-    
+
 def main(**_) -> None:
     grant_access_to_catalog()
 
